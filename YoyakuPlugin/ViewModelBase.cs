d8956d72b22b6971c78e047c467e21c70f664c4b
Ryu Shinmura <rysestock@gmail.com>
2024-09-11 18:55:45 +0900

    予約管理プラグインを移植

---
+++ YoyakuPlugin/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace YoyakuPlugin;
+
+public class ViewModelBase
+{
+    #region INotifyPropertyChanged
+    [NonSerialized]
+    private PropertyChangedEventHandler? _propertyChanged;
+    /// <summary>
+    /// 
+    /// </summary>
+    public event PropertyChangedEventHandler? PropertyChanged
+    {
+        add { _propertyChanged += value; }
+        remove { _propertyChanged -= value; }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="propertyName"></param>
+    protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        _propertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    #endregion
+}
